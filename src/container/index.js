033c465c7efcf5e9932111eaf9d71234e176412f
qusai1999 <105935241+qusai1999@users.noreply.github.com>
2023-01-10 23:59:10 +0100

    first commit

---
+++ src/container/index.js
@@ -0,0 +1,13 @@
+import AboutUs from './AboutUs/AboutUs';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Intro from './Intro/Intro';
+import SpecialMenu from './Menu/SpecialMenu';
+
+export {
+  AboutUs,
+  Footer,
+  Header,
+  Intro,
+  SpecialMenu,
+};
